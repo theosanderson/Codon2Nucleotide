01c6fabaf6d8e428d153c50878ca715d8d8ff12b
theosanderson <theo@theo.io>
2021-10-22 22:13:18 +0100

    Add ORF1a or B mode

--- src/App.js
+++ src/App.js
@@ -24,9 +24,15 @@ const genes = {
 
 function App() {
   let [codon, setCodon] = useState(484);
+
   let [nucleotide, setNucleotide] = useState(null);
   let [gene, setGene] = useState("S")
+  let [themode,setthemode] = useState("nucleotide")
   let end_nucleotide = ""
+
+
+
+
   if (nucleotide == null) {
     const start_nucleotide = genes[gene][0] + (codon - 1) * 3
     end_nucleotide = start_nucleotide + 2
@@ -48,11 +54,55 @@ function App() {
     }
   }
 
+  let [orf1aorb, setOrf1aOrb] = useState(null);
+  let [orf1aorbcodon, setORF1aorBCodon] = useState(null);
+  let [combinedCodon,setCombinedCodon] = useState(5005);
+
+  if (combinedCodon == null) {
+    combinedCodon = orf1aorbcodon + (orf1aorb === "ORF1b" ? 4401 : 0)
+  }
+  else{
+    if(combinedCodon > 4401){
+      orf1aorbcodon = combinedCodon - 4401
+      orf1aorb = "ORF1b"
+    }
+    else{
+      orf1aorb = "ORF1a"
+      orf1aorbcodon = combinedCodon
+      
+    }
+  }
+
+  if (themode === "nucleotide") {
+    if( ["ORF1a","ORF1b"].includes(gene)){
+      if (orf1aorb !== gene){
+      setOrf1aOrb(gene)
+      setCombinedCodon(null)
+    }
+      if(orf1aorbcodon !== codon){
+      setORF1aorBCodon(codon)
+      setCombinedCodon(null)
+      }
+    }
+
+  }
+  else{
+    if(gene!==orf1aorb){
+      setGene(orf1aorb)
+      setNucleotide(null)
+    }
+    if (codon !== orf1aorbcodon){
+      setCodon(orf1aorbcodon)
+      setNucleotide(null)
+    }
+    
+  }
 
   return (
     <div className="App">
       <h1>Codon2Nucleotide</h1>
       <p>Convert from codon position to genomic coordinates, or vice versa. Currently for SARS-CoV-2. <a href="https://github.com/theosanderson/codon2nucleotide">GitHub</a></p>
+      {themode==="nucleotide"&&
       <div className="flex-container">
 
         <div className="flex-child magenta">
@@ -78,8 +128,47 @@ function App() {
           Nucleotide: <input type="number" value={nucleotide} onChange={e => { setNucleotide(parseInt(e.target.value)); setCodon(null); }}></input>
           <div className="to_sec">{end_nucleotide && " to " + end_nucleotide}</div>
         </div>
+        
 
       </div>
+}
+
+      {themode==="orf1ab"&&
+      <div className="flex-container">
+
+
+<div className="flex-child magenta">
+
+  <h3>ORF1a or B </h3>
+  <div style={{ marginBottom: "5px" }}> Gene: <select value={orf1aorb} onChange={e => { setOrf1aOrb(e.target.value); setORF1aorBCodon(codon); setCombinedCodon(null) ;  }}>
+    {['ORF1a','ORF1b'].sort().map(x =>
+      <option key={x} value={x}>{x}</option>
+
+
+
+    )}
+  </select></div>
+
+  Codon: <input type="number" value={orf1aorbcodon} onChange={e => { setORF1aorBCodon(parseInt(e.target.value)); setOrf1aOrb(orf1aorb) ;setCombinedCodon(null) ;}}></input>
+
+
+
+</div>
+
+<div className="flex-child green">
+  <h3>ORF1ab </h3>
+  Codon: <input type="number" value={combinedCodon} onChange={e => { setCombinedCodon(parseInt(e.target.value)); setORF1aorBCodon(null);  }}></input>
+
+</div>
+
+
+</div>
+}
+
+<p><button onClick={()=> {
+  setthemode(themode==="nucleotide"?"orf1ab":"nucleotide")
+}}>{themode==="orf1ab"?  <span>Switch to codon / nucleotide mode</span>: <span>Switch to ORF1a/b:ORF1ab conversion</span>}</button></p>
+
     </div >
   );
 }
